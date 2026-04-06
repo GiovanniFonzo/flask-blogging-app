450bfb765cca1fc65ecfce9376b2f047d5e53a0c
Giovanni Fonzo <username@users.noreply.github.com>
2026-04-06 18:21:51 +0100

    feat(api): configure Flask backend and PostgreSQL database setup

--- backend.py
+++ backend.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = Flask(__name__)
+
+DATABASE_URL = "postgresql+psycopg2://giovannifonzo:Dublin.456@localhost/blogging_app"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
 
 @app.route("/health")
 def health():
     return {"status": "ok"}
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
